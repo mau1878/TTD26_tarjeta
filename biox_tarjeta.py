172e4c72cba492b1c57a5204b0ab3ffebff67721
MTaurus <97911761+mau1878@users.noreply.github.com>
2025-10-17 21:33:44 -0300

    Update biox_tarjeta.py

--- biox_tarjeta.py
+++ biox_tarjeta.py
@@ -6,7 +6,7 @@ import os
 st.title("Emisor de tarjetas para miembros platino del Círculo Sagrado de Empomados por BioX")
 
 # Cargar la imagen proporcionada
-image_path = "biox_tarjeta.png"
+image_path = "BIOX.png"
 if os.path.exists(image_path):
     img = Image.open(image_path).convert("RGBA")
 else:
